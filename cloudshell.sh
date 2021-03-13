8d67de2c52a8021372b780785f4c23f2d3d1b7fa
Samsul Hadi <samsulhadi@Samsuls-MacBook-Air-2.local>
2021-03-13 18:01:54 +0700

    cloudshell

--- cloudshell.sh
+++ cloudshell.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-gcloud auth revoke --all
-
-while [[ -z "$(gcloud config get-value core/account)" ]]; 
-do echo "waiting login" && sleep 2; 
-done
-
-while [[ -z "$(gcloud config get-value project)" ]]; 
-do echo "waiting project" && sleep 2; 
-done
 
 export PROJECT_ID=$(gcloud info --format='value(config.project)')
 gsutil mb gs://$PROJECT_ID
